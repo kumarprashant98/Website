9160b3181522504cc0ea5ec980a74aa8cc208062
kumarprashant98 <prashantkumarpk000@gmail.com>
2023-05-22 18:32:31 +0530

    First Website

---
+++ sum.js
@@ -0,0 +1,9 @@
+var name = prompt("Please enter your name:");
+    alert("Hello, " + name + "! Welcome!");
+    var num1 = parseFloat(prompt("Enter the first number:"));
+    var num2 = parseFloat(prompt("Enter the second number:"));
+    var sum = num1 + num2;
+    alert("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    function showAlert(){
+        alert("Button Clicked" );
+    }
